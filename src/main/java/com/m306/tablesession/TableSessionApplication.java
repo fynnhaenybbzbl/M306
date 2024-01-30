8381a3b70e8ae5cb94f8ce0fb33401bd5fc022ab
fynnhaenybbzbl <108476679+fynnhaenybbzbl@users.noreply.github.com>
2024-01-30 08:21:38 +0100

    Add existing project files to Git

---
+++ src/main/java/com/m306/tablesession/TableSessionApplication.java
@@ -0,0 +1,13 @@
+package com.m306.tablesession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableSessionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/m306/tablesession/TableSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m306.tablesession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
