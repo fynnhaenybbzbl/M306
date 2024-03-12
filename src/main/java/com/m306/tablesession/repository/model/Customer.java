dae90646cb2044b28ce19005cf06c40dd6f1dc7b
Kiimi27 <kimi.schweingruber@bbzbl-it.ch>
2024-03-12 09:33:13 +0100

    thymeleaf show customers

--- src/main/java/com/m306/tablesession/repository/model/Customer.java
+++ src/main/java/com/m306/tablesession/repository/model/Customer.java
@@ -1,6 +1,8 @@
 package com.m306.tablesession.repository.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,6 +18,7 @@ import java.util.Date;
 @Validated
 public class Customer {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
