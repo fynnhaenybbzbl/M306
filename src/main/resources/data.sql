4b24d98fca25b24a0f1dd6aecb67ba38df32121f
fynnhaenybbzbl <108476679+fynnhaenybbzbl@users.noreply.github.com>
2024-03-19 09:39:27 +0100

    add css for html + remove h2

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO customer (id, first_name, last_name, birthdate, street, street_number, city, zip) VALUES (1, 'Max', 'Mustermann', '1990-01-01', 'Musterstraße', '1', 'Musterstadt', '12345');
-
-INSERT INTO employee (username, role, password) VALUES ('User', 'USER', 'user');
-INSERT INTO employee (username, role, password) VALUES ('Admin', 'ADMIN', 'admin');
